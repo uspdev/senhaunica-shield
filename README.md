66b4e1c688f55f29a0c1060184e8d65bf9b6a873
Masaki K Neto <kawabata@usp.br>
2023-05-03 16:44:07 -0300

    Substituindo biblioteca de oauth #18

--- README.md
+++ README.md
@@ -1,28 +1,52 @@
 # Senhaunica
 Biblioteca genérica para integrar senha única em PHP
 
+## Changelog
+
+3/5/2023: versão 2.0
+* nova versão agora usa a biblioteca league/oauth1-client
+* compatível com php 8
+* Chamada agora é estática
+
 ## Dependência
 
-biblioteca zorrodg/oauth-php  
-biblioteca ext-curl
-
+* biblioteca league/oauth1-client
+* PHP >=7.1 | >=8.0
 ## Instalação
 
 Se seu projeto não usa composer ainda, é uma boa idéia começar a usá-lo.
 
 ```
-composer init
-composer require uspdev/senhaunica
-composer install
+composer require uspdev/senhaunica:2.0
 ```
 
 ## Uso
 
-Esta biblioteca foi testada em debian 10, debian 9, ubuntu 20.04, ubuntu 18.04 e ubuntu 16.04.
+Esta biblioteca foi testada em debian 10 e ubuntu 22.04.
 
-O token pode ser usado para várias aplicações por meio do callback_id cadastrado em https://dev.uspdigital.usp.br/adminws/
+O token pode ser usado para várias aplicações por meio do callback_id cadastrado em https://uspdigital.usp.br/adminws/oauthConsumidorAcessar
 
 Deve-se criar uma rota (/loginusp por exemplo) com o seguinte código:
+
+```php
+require_once __DIR__.'/vendor/autoload.php';
+session_start();
+
+use Uspdev\Senhaunica\Senhaunica;
+
+$clientCredentials = [
+    'identifier' => 'identificacao',
+    'secret' => 'chave-secreta',
+    'callback_id' => 0,
+];
+
+Senhaunica::login($clientCredentials);
+
+header('Location:../');
+exit;
+```
+
+Opcionalmente você pode passar os parâmetros via `env`:
 
 ```php
 require_once __DIR__.'/vendor/autoload.php';
@@ -34,25 +58,50 @@ putenv('SENHAUNICA_KEY=');
 putenv('SENHAUNICA_SECRET=');
 putenv('SENHAUNICA_CALLBACK_ID=');
 
-# se for usar ambiente de testes use
-# putenv('SENHAUNICA_DEV=');
+Senhaunica::login();
 
-$auth = new Senhaunica();
-$res = $auth->login();
-
-echo '<pre>';
-print_r($res);
-echo '</pre>';
-
-header('Location:/alguma_rota');
-
+header('Location:../');
+exit;
 ```
 
-Se você quiser, por exemplo, validar o vínculo do login, use o código abaixo. Ele irá retornar o primeiro vínculo que encontrar dentro da lista fornecida. Ao invés de usar `tipoVinculo` você pode usar qualquer variável dentro do array de vínculos.
+Os dados do usuário autenticado estarão em `$_SESSION['oauth_user']`
+
+Ele também pode ser recuperado usando
+
+    $user = Uspdev\Senhaunica\Senhaunica::getUserDetail();
+
+Ele contém um array com todos os dados retornados do oauth. Exemplo:
+
+    [loginUsuario] => 111111
+    [nomeUsuario] => Jose Maria da Silva
+    [tipoUsuario] => I
+    [emailPrincipalUsuario] => email@usp.br
+    [emailAlternativoUsuario] => email-alternativo@gmail.com
+    [emailUspUsuario] => outro-email@usp.br
+    [numeroTelefoneFormatado] => (0xx16)1234-5678 - ramal USP: 345678
+    [wsuserid] => Iasdkughacsdghçalekhagsghaegawe
+    [vinculo] => Array
+        (
+            [0] => Array
+                (
+                    [tipoVinculo] => SERVIDOR
+                    [codigoSetor] => 000
+                    [nomeAbreviadoSetor] => ABC
+                    [nomeSetor] => Meu setor
+                    [codigoUnidade] => 18
+                    [siglaUnidade] => EESC
+                    [nomeUnidade] => Escola de Engenharia de São Carlos
+                    [nomeVinculo] => Servidor
+                    [nomeAbreviadoFuncao] => Minha função
+                    [tipoFuncao] => Informática
+                )
+
+        )
+
+Adicionalmente, se você quiser validar o vínculo do login, use o código abaixo. Ele irá retornar o primeiro vínculo que encontrar dentro da lista fornecida. Ao invés de usar `tipoVinculo` você pode usar qualquer variável dentro do array de vínculos.
 
 ```php
-$vinculo = $auth->obterVinculo('tipoVinculo', ['SERVIDOR','OUTRO_VINCULO', '...']);
+$campo = 'tipoVinculo';
+$valores = ['SERVIDOR','OUTRO_VINCULO', '...'];
+$vinculo = Uspdev\Senhaunica\Senhaunica::obterVinculo($campo, $valores);
 ```
-
-OBS: 7/8/2020 - atualizado para utilizar variáveis de ambiente com os parâmetros do oauth,
-tornado obsoleto o uso de array de configuração no contrutor. 
--- src/Senhaunica.php
+++ src/Senhaunica.php
@@ -1,106 +1,71 @@
 <?php
 
+namespace Uspdev\Senhaunica;
+
+use Uspdev\Senhaunica\ServerUSP;
+
 /**
- * Senhaunica: classe para autenticar contra senha única da USP
+ * Senhaunica: classe para autenticar senha única da USP
  *
  * @author masakik
  *
  */
-
-namespace Uspdev\Senhaunica;
-
 class Senhaunica
 {
-
-    protected $curl_options = array(
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    );
-
-    protected $options;
-
-    protected $user_data_url;
-
-    public function __construct($oauth = '')
+    public static function login($clientCredentials = [])
     {
-        if (empty($oauth)) {
-
-            if (empty(getenv('SENHAUNICA_KEY')) || empty(getenv('SENHAUNICA_SECRET'))) {
-                die('Variáveis de ambiente não configurados');
-            }
-            //vamos caregar por variáveis de ambiente
-            $oauth_host = (getenv('SENHAUNICA_DEV') == 'yes') ?
-                'https://dev.uspdigital.usp.br' : 'https://uspdigital.usp.br';
-
-            $oauth = [
-                'consumer_key' => getenv('SENHAUNICA_KEY'),
-                'consumer_secret' => getenv('SENHAUNICA_SECRET'),
-                'callback_id' => getenv('SENHAUNICA_CALLBACK_ID'),
-            ];
-        } else {
-            // vamos avisar para usar variáveis de ambiente mas permite
-            // continuar usando parametro no construtor
-            //\trigger_error('Config por array esta obsoleto: use variaveis de ambiente.', E_USER_WARNING);
-
-            if ($oauth['amb'] == 1 || $oauth['amb'] == 'dev') {
-                $oauth_host = 'https://dev.uspdigital.usp.br'; // ambiente de teste
-            } elseif ($oauth['amb'] == 2 || $oauth['amb'] == 'prod') {
-                $oauth_host = 'https://uspdigital.usp.br'; // ambiente de producao
-            } else {
-                echo 'Ambiente não configurado!';
-                exit;
-            }
+        if (empty($clientCredentials)) {
+            $clientCredentials['identifier'] = getenv('SENHAUNICA_KEY');
+            $clientCredentials['secret'] = getenv('SENHAUNICA_SECRET');
+            $clientCredentials['callback_id'] = getenv('SENHAUNICA_CALLBACK_ID');
         }
 
-        $this->user_data_url = $oauth_host . '/wsusuario/oauth/usuariousp';
+        $server = new ServerUSP($clientCredentials);
 
-        //  Init the OAuthStore
-        $this->options = array(
-            'consumer_key' => $oauth['consumer_key'],
-            'consumer_secret' => $oauth['consumer_secret'],
-            'server_uri' => $oauth_host,
-            'request_token_uri' => $oauth_host . '/wsusuario/oauth/request_token',
-            'authorize_uri' => $oauth_host . '/wsusuario/oauth/authorize',
-            'access_token_uri' => $oauth_host . '/wsusuario/oauth/access_token',
-        );
-
-        // se tiver setado callback id use ele
-        $this->callback_param = (isset($oauth['callback_id'])) ? '&callback_id=' . $oauth['callback_id'] : '';
-    }
-
-    public function login()
-    {
-        // se já estiver logado vamos mostrar os dados
-        if (!empty($_SESSION['oauth_user'])) {
+        // step 3: tudo ok
+        if (isset($_SESSION['token_credentials'])) {
+            $tokenCredentials = unserialize($_SESSION['token_credentials']);
+            $_SESSION['oauth_user'] = $server->getUserDetails($tokenCredentials);
+            session_write_close();
             return $_SESSION['oauth_user'];
         }
 
-        // se não vamos obter no oauth
-        \OAuth1\OAuthStore::instance('Session', $this->options);
-
-        if (empty($_GET['oauth_token'])) {
-            //  STEP 1:  If we do not have an OAuth token yet, go get one
-            $this->getOauthToken();
-        } else {
-            //  STEP 2:  Get an access token
-            $this->getAccessToken();
-            $_SESSION['oauth_user'] = $this->getUserInfo();
-            return $_SESSION['oauth_user'];
+        // step 2: recebendo o retorno do oauth
+        if (isset($_GET['oauth_token']) && isset($_GET['oauth_verifier'])) {
+            $temporaryCredentials = unserialize($_SESSION['temporary_credentials']);
+            $tokenCredentials = $server->getTokenCredentials($temporaryCredentials, $_GET['oauth_token'], $_GET['oauth_verifier']);
+            unset($_SESSION['temporary_credentials']);
+            $_SESSION['token_credentials'] = serialize($tokenCredentials);
+            $_SESSION['oauth_user'] = $server->getUserDetails($tokenCredentials);
+            session_write_close();
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            exit;
         }
+
+        // step 1: credenciais temporárias e redirecionamento para login USP
+        $temporaryCredentials = $server->getTemporaryCredentials();
+        $_SESSION['temporary_credentials'] = serialize($temporaryCredentials);
+        session_write_close();
+        $url = $server->getAuthorizationUrl($temporaryCredentials) . '&callback_id=' . $clientCredentials['callback_id'];
+        header('Location: ' . $url);
+        exit;
     }
 
-    public function logout()
+    public static function getUserDetail()
     {
-        // aqui simplesmente esquece as variáveis do oauth da session
-        unset($_SESSION['oauth_user']);
-        unset($_SESSION['oauth_' . $this->options['consumer_key']]);
-        return true;
+        return isset($_SESSION['oauth_user']) ? $_SESSION['oauth_user'] : null;
     }
 
-    // retorna o primeiro vinculo que encontrar com o critério
-    // 'campo' == [valor1, ou valor2, ...]
-    public function obterVinculo($campo, $valores)
+    /**
+     * retorna o primeiro vinculo que encontrar
+     *
+     * com o critério 'campo' == [valor1, ou valor2, ...]
+     */
+    public static function obterVinculo($campo, $valores)
     {
+        if (!isset($_SESSION['oauth_user']['vinculo'])) {
+            return null;
+        }
         if (!is_array($valores)) {
             $valores = [$valores];
         }
@@ -112,58 +77,5 @@ class Senhaunica
             }
         }
         return false;
-    }
-
-    protected function getOauthToken()
-    {
-        try {
-            $tokenResultParams = \OAuth1\OAuthRequester::requestRequestToken(
-                $this->options['consumer_key'], null, null, 'POST', null, $this->curl_options
-            );
-        } catch (\OAuth1\OAuthException2 $e) {
-            echo "OAuthException in (fase 1) requestRequestToken:  " . $e->getMessage();
-            var_dump($e);
-            exit;
-        }
-        // vamos direcionar o usuário para a tela de autenticação
-        header('Location: ' . $this->options['authorize_uri'] .
-            '?oauth_token=' . $tokenResultParams['token'] . $this->callback_param
-        );
-        exit;
-    }
-
-    protected function getAccessToken()
-    {
-        $oauthToken = $_GET['oauth_token'];
-        $oauthVerifier = $_GET['oauth_verifier'];
-        $tokenResultParams = $_GET;
-
-        try {
-            \OAuth1\OAuthRequester::requestAccessToken(
-                $this->options['consumer_key'], $oauthToken, 0, 'POST', $_GET, $this->curl_options
-            );
-        } catch (\OAuth1\OAuthException2 $e) {
-            echo "OAuthException in (fase 2) requestAccessToken:  " . $e->getMessage();
-            var_dump($e);
-            // Something wrong with the oauth_token.
-            // Could be:
-            // 1. Was already ok
-            // 2. We were not authorized
-            exit;
-        }
-    }
-
-    protected function getUserInfo()
-    {
-        $request = new \OAuth1\OAuthRequester($this->user_data_url, 'POST');
-        $result = $request->doRequest(null, $this->curl_options);
-        if ($result['code'] == 200) {
-            $loginUSP = json_decode($result['body'], true);
-            return $loginUSP;
-        } else {
-            echo 'Error: result code not 200';
-            print_r($result);
-            exit;
-        }
     }
 }
