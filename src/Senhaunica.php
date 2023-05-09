244646062872a200d9a45daeb78f99e59cfd4fea
Masaki K Neto <kawabata@usp.br>
2023-05-09 10:46:36 -0300

    retornando user no step 2

--- src/Senhaunica.php
+++ src/Senhaunica.php
@@ -22,16 +22,17 @@ class Senhaunica
      * 
      * Processo do oauth: 
      * primeiro processa step 1, que direciona o usuário para a página de login do Oauth;
-     * o retorno do oauth é direcionado para cá novamente que processa step 2;
-     * o step 2 redireciona para cá novamente que processa step 3 e 
-     * retorna os dados do usuário autenticado para a rota que chamou inicialmente
+     * o retorno do oauth é direcionado para cá novamente que processa step 2 e reotrna dados do oauth;
+     * o step 3 é para o caso de solicitar o login novamente porém já estiver autenticado. 
+     *   Nesse caso pega as informações do usuário novamente e 
+     *   retorna as informações para a rota que chamou inicialmente
      * 
      * @param $clientCredentials
      * @return Null|Array
      */
     public static function login($clientCredentials = [])
     {
-        if (!session_id()) {
+        if(session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
 
@@ -59,8 +60,7 @@ class Senhaunica
             unset($_SESSION[self::NS]['temporary_credentials']);
             $_SESSION[self::NS]['token_credentials'] = serialize($tokenCredentials);
             $_SESSION[self::NS]['oauth_user'] = $server->getUserDetails($tokenCredentials);
-            header('Location: ' . $_SERVER['PHP_SELF']);
-            exit;
+            return $_SESSION[self::NS]['oauth_user'];
         }
 
         // step 1: credenciais temporárias e redirecionamento para login USP
@@ -71,6 +71,18 @@ class Senhaunica
         exit;
     }
 
+    /**
+     * Limpa as variáveis de sessão do oauth para forçar novamente a autenticação.
+     */
+    public static function logout() {
+        unset($_SESSION[self::NS]);
+    }
+
+    /**
+     * Retorna os dados do usuário que estão na session ou null se não disponível
+     * 
+     * Fica para a aplicação decidir se vai fazer login novamente ou tratar de outra forma.
+     */
     public static function getUserDetail()
     {
         return isset($_SESSION[self::NS]['oauth_user']) ? $_SESSION[self::NS]['oauth_user'] : null;
