5acba4028eead06c49174fbed2be2d69fef8ddce
Daniel Picon <dpicon@sc.usp.br>
2025-02-05 10:03:15 -0300

    Prevenindo tentativa de login com usuário já logado

--- src/SenhaunicaShield.php
+++ src/SenhaunicaShield.php
@@ -9,48 +9,51 @@ class SenhaunicaShield
 {
     public static function login($clientCredentials = [])
     {
-        // Carregar o serviço de sessão do CodeIgniter
-        $session = Services::session();
+        // Só executa se não há usuário conectado
+        if (!auth()->loggedIn()) {
+            // Carregar o serviço de sessão do CodeIgniter
+            $session = Services::session();
 
-        if (empty($clientCredentials)) {
-            $clientCredentials['identifier'] = getenv('SENHAUNICA_KEY');
-            $clientCredentials['secret'] = getenv('SENHAUNICA_SECRET');
-            $clientCredentials['callback_id'] = getenv('SENHAUNICA_CALLBACK_ID');
-        }
+            if (empty($clientCredentials)) {
+                $clientCredentials['identifier'] = getenv('SENHAUNICA_KEY');
+                $clientCredentials['secret'] = getenv('SENHAUNICA_SECRET');
+                $clientCredentials['callback_id'] = getenv('SENHAUNICA_CALLBACK_ID');
+            }
 
-        $server = new ServerUSP($clientCredentials);
+            $server = new ServerUSP($clientCredentials);
 
-        // step 3: tudo ok
-        if ($session->has('token_credentials')) {
-            $tokenCredentials = unserialize($session->get('token_credentials'));
-            $userDetails = $server->getUserDetails($tokenCredentials);
-            Login::authenticate($userDetails);
-            return auth()->user();
-        }
+            // step 3: tudo ok
+            if ($session->has('token_credentials')) {
+                $tokenCredentials = unserialize($session->get('token_credentials'));
+                $userDetails = $server->getUserDetails($tokenCredentials);
+                Login::authenticate($userDetails);
+                return auth()->user();
+            }
 
-        // step 2: recebendo o retorno do oauth
-        if (isset($_GET['oauth_token']) && isset($_GET['oauth_verifier'])) {
-            $temporaryCredentials = unserialize($session->get('temporary_credentials'));
-            $tokenCredentials = $server->getTokenCredentials($temporaryCredentials, $_GET['oauth_token'], $_GET['oauth_verifier']);
+            // step 2: recebendo o retorno do oauth
+            if (isset($_GET['oauth_token']) && isset($_GET['oauth_verifier'])) {
+                $temporaryCredentials = unserialize($session->get('temporary_credentials'));
+                $tokenCredentials = $server->getTokenCredentials($temporaryCredentials, $_GET['oauth_token'], $_GET['oauth_verifier']);
 
-            // Atualizando as credenciais de sessão
-            $session->remove('temporary_credentials');
-            $session->set('token_credentials', serialize($tokenCredentials));
-            $session->set('oauth_user', $server->getUserDetails($tokenCredentials));
+                // Atualizando as credenciais de sessão
+                $session->remove('temporary_credentials');
+                $session->set('token_credentials', serialize($tokenCredentials));
+                $session->set('oauth_user', $server->getUserDetails($tokenCredentials));
 
-            // Redirecionamento após autenticação
-            header('Location: ' . $_SERVER['PHP_SELF']);
+                // Redirecionamento após autenticação
+                header('Location: ' . $_SERVER['PHP_SELF']);
+                exit;
+            }
+
+            // step 1: credenciais temporárias e redirecionamento para login USP
+            $temporaryCredentials = $server->getTemporaryCredentials();
+            $session->set('temporary_credentials', serialize($temporaryCredentials));
+            $url = $server->getAuthorizationUrl($temporaryCredentials) . '&callback_id=' . $clientCredentials['callback_id'];
+
+            // Redirecionamento para o URL de autorização
+            header('Location: ' . $url);
             exit;
         }
-
-        // step 1: credenciais temporárias e redirecionamento para login USP
-        $temporaryCredentials = $server->getTemporaryCredentials();
-        $session->set('temporary_credentials', serialize($temporaryCredentials));
-        $url = $server->getAuthorizationUrl($temporaryCredentials) . '&callback_id=' . $clientCredentials['callback_id'];
-
-        // Redirecionamento para o URL de autorização
-        header('Location: ' . $url);
-        exit;
     }
 
     public static function getUserDetail()
