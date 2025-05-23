737776b932037666d80355010010484b18951852
Daniel Picon <dpicon@sc.usp.br>
2025-05-23 11:01:15 -0300

    Criando Migration para inserir nome completo do usuário na tabela users

---
+++ src/Resources/Migrations/2024-05-22-000000_AddNameToUsers.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Forge;
+use CodeIgniter\Database\Migration;
+
+class AddNameToUsers extends Migration
+{
+    public function __construct(?Forge $forge = null)
+    {
+        parent::__construct($forge);
+
+        /** @var \Config\Auth $authConfig */
+        $authConfig   = config('Auth');
+        $this->tables = $authConfig->tables;
+    }
+
+    public function up()
+    {
+        $fields = [
+            'fullname' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+        ];
+        $this->forge->addColumn($this->tables['users'], $fields);
+        $this->forge->addColumn();
+    }
+
+    public function down()
+    {
+        $fields = [
+            'fullname',
+        ];
+        $this->forge->dropColumn($this->tables['users'], $fields);
+    }
+}
