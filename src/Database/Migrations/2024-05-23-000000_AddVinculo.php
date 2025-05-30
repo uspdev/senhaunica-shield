173c252f039a785a9242cb527e977bd6ef1eb04a
Daniel Picon <dpicon@sc.usp.br>
2025-05-30 10:23:50 -0300

    Migration para novos campos de vínculo

---
+++ src/Database/Migrations/2024-05-23-000000_AddVinculo.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Uspdev\SenhaunicaShield\Resources\Migrations;
+
+use CodeIgniter\Database\Forge;
+use CodeIgniter\Database\Migration;
+
+class AddNameToUsers extends Migration
+{
+    public function __construct(?Forge $forge = null)
+    {
+        parent::__construct($forge);
+
+        /** @var \Config\Auth $authConfig */
+        $authConfig   = config('Auth');
+        $this->tables = $authConfig->tables;
+    }
+
+    public function up()
+    {
+        $fields = [
+            'tipoVinculo' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'codigoSetor' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'nomeAbreviadoSetor' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'nomeSetor' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'codigoUnidade' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'siglaUnidade' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'nomeUnidade' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'nomeVinculo' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'nomeAbreviadoFuncao' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+            'tipoFuncao' => ['type' => 'VARCHAR', 'constraint' => '255', 'null' => true],
+        ];
+        $this->forge->addColumn($this->tables['users'], $fields);
+    }
+
+    public function down()
+    {
+        $fields = [
+            'tipoVinculo',
+            'codigoSetor',
+            'nomeAbreviadoSetor',
+            'nomeSetor',
+            'codigoUnidade',
+            'siglaUnidade',
+            'nomeUnidade',
+            'nomeVinculo',
+            'nomeAbreviadoFuncao',
+            'tipoFuncao',
+        ];
+        $this->forge->dropColumn($this->tables['users'], $fields);
+    }
+}
