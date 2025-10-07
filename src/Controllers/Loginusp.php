dd554412edbe3da38327689147fc10f4e96dc75c
Daniel Picon <dpicon@sc.usp.br>
2025-10-07 15:07:52 -0300

    Adicionando validação das credenciais de conexão no método loginusp

--- src/Controllers/Loginusp.php
+++ src/Controllers/Loginusp.php
@@ -8,6 +8,14 @@ class Loginusp extends BaseController
 {
     public function loginusp()
     {
+        $key = getenv('SENHAUNICA_KEY');
+        $secret = getenv('SENHAUNICA_SECRET');
+        $callback = getenv('SENHAUNICA_CALLBACK_ID');
+
+        if (!$key || !$secret || !$callback) {
+            return redirect()->to('login')->with('error', 'Falta configurar as credenciais para a conexão com o auth USP');
+        }
+
         SenhaunicaShield::login();
         header('Location:../');
         exit;
